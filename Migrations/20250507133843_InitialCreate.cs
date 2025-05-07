1ca08342ce82601ec5d2d7bf10dac34c061969be
Berco Andrei <andrei2003berco@gmail.com>
2025-05-07 16:45:37 +0300

    Add migrations

---
+++ Migrations/20250507133843_InitialCreate.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PBL_IoT_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "PBL_IOT");
+
+            migrationBuilder.CreateTable(
+                name: "SensorTypes",
+                schema: "PBL_IOT",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "PBL_IOT",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plants",
+                schema: "PBL_IOT",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    SensorIds = table.Column<List<int>>(type: "integer[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plants", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Plants_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "PBL_IOT",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sensors",
+                schema: "PBL_IOT",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: true),
+                    SensorTypeId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sensors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sensors_SensorTypes_SensorTypeId",
+                        column: x => x.SensorTypeId,
+                        principalSchema: "PBL_IOT",
+                        principalTable: "SensorTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sensors_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "PBL_IOT",
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SensorReadings",
+                schema: "PBL_IOT",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SensorId = table.Column<int>(type: "integer", nullable: false),
+                    Value = table.Column<double>(type: "double precision", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorReadings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SensorReadings_Sensors_SensorId",
+                        column: x => x.SensorId,
+                        principalSchema: "PBL_IOT",
+                        principalTable: "Sensors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plants_UserId",
+                schema: "PBL_IOT",
+                table: "Plants",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorReadings_SensorId",
+                schema: "PBL_IOT",
+                table: "SensorReadings",
+                column: "SensorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sensors_SensorTypeId",
+                schema: "PBL_IOT",
+                table: "Sensors",
+                column: "SensorTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sensors_UserId",
+                schema: "PBL_IOT",
+                table: "Sensors",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Plants",
+                schema: "PBL_IOT");
+
+            migrationBuilder.DropTable(
+                name: "SensorReadings",
+                schema: "PBL_IOT");
+
+            migrationBuilder.DropTable(
+                name: "Sensors",
+                schema: "PBL_IOT");
+
+            migrationBuilder.DropTable(
+                name: "SensorTypes",
+                schema: "PBL_IOT");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "PBL_IOT");
+        }
+    }
+}
